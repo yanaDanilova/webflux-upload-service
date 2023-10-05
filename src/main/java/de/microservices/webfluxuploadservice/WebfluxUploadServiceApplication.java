caf3299511c1c647cd6d8f625baa8df1a95b449e
yanad <yana.danilova1509@gmail.com>
2023-10-05 12:38:34 +0200

    Initial commit

---
+++ src/main/java/de/microservices/webfluxuploadservice/WebfluxUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package de.microservices.webfluxuploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxUploadServiceApplication.class, args);
+	}
+
+}
